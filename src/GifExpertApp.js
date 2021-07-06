4fb8238e6306670ac3150393edce1f4e10f987a6
Sergi Mas <jflepetit@gmail.com>
2021-07-06 12:41:45 +0200

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    // const handleAdd = () => {
+    //     setCategories( [...categories, 'HunterXHunter'] );
+    // }
+
+    return (
+        <>
+            <h2>GifExpert</h2>
+            <AddCategory setCategories={ setCategories } />
+            <hr />
+
+            <ol>
+                { 
+                    categories.map( category => 
+                        <GifGrid
+                            key={ category }
+                            category={ category } />
+                    )
+                }
+            </ol>
+        </>
+    )
+}
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+
+
+export const getGifs = async ( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=Judj5RUdTlGdYa9d6phCWNHLbOiOuPgm`;
+    const respuesta = await fetch( url );
+    const { data } = await respuesta.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+
+    return gifs;
+};
